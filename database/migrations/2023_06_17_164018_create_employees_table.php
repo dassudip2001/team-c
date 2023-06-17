00fc16e6781100128e10a8cfa3fba903ed804b8f
Sudip Das <sudip5428@gmail.com>
2023-06-17 22:22:30 +0530

    code commit for model generate

--- database/migrations/2023_06_17_164018_create_employees_table.php
+++ database/migrations/2023_06_17_164018_create_employees_table.php
@@ -13,6 +13,15 @@ return new class extends Migration
     {
         Schema::create('employees', function (Blueprint $table) {
             $table->id();
+            $table->string('fast_name',100);
+            $table->string('last_name',100);
+            $table->string('email')->unique()->index();
+            $table->date('date_of_birth');
+            $table->string('education');
+            $table->string('gender');
+            $table->string('company',210);
+            $table->float('packages');
+            $table->float('experience');
             $table->timestamps();
         });
     }
